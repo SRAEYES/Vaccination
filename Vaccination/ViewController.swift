70af910a0f60e235bae9e9509e8a8ee26297746f
SRAEYES <chakkasraeyes05@gmail.com>
2025-12-10 00:48:30 +0530

    Initial Commit

---
+++ Vaccination/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vaccination
+//
+//  Created by user66 on 10/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VaccinationTests/VaccinationTests.swift
@@ -0,0 +1,17 @@
+//
+//  VaccinationTests.swift
+//  VaccinationTests
+//
+//  Created by user66 on 10/12/25.
+//
+
+import Testing
+@testable import Vaccination
+
+struct VaccinationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
